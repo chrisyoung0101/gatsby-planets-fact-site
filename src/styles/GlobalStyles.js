88e1b2bbe0432229f917e4f1cda3b13d768b7436
Chris Young <chrisyoung0101@gmail.com>
2021-06-25 05:30:25 -0500

    import bg.svg

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -4,9 +4,18 @@ import { createGlobalStyle } from 'styled-components';
 // :root allows this to be available anywhere in the app
 const GlobalStyles = createGlobalStyle`
 
+ /* this makes sure that all our stuff goes through Gatsby although it is not regulary valid to put
+ here like this */
+
+import bg from '../assets/images/bg.svg';
+
 /* makes styling our widths much easier */
 * {
   box-sizing: border-box;
+}
+
+html {
+  background-image: url(${bg});
 }
 
 /* remove all margin and padding so we can butt content up to the upper left corner of the screen */
